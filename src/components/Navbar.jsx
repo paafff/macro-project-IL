104d627febc4910854777ab8600f748d0e5f086f
IlhamSoejudAlkahfiardy <ilhamyusuf386@gmail.com>
2023-09-18 20:24:52 +0700

    add navbar styles

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useState, useEffect } from 'react'
+import '../dist/css/navbar.css'
 // import ppSaya from '../assets/ppSaya.png';
 // import { HiOutlineBeaker } from 'react-icons/hi';
 
@@ -13,13 +15,28 @@ import { Link } from 'react-router-dom';
 // } from 'react-icons/bs';
 
 const Navbar = () => {
+
+  const [shadowNavbar, setShadowNavbar] = useState(false);
+
+  const changeShadowNavbar = () => {
+    if (window.scrollY > 10) {
+      setShadowNavbar(true);
+    } else {
+      setShadowNavbar(false);
+    }
+  }
+
+  useEffect(() => {
+    window.addEventListener('scroll', changeShadowNavbar);
+  })
+
   return (
-    <nav className="bg-none">
-      <div className="max-w-4xl mx-auto p-4 sm:px-6 lg:px-8">
-        <div className="flex items-center justify-between h-16">
+    <nav className={`bg-slate-200 fixed w-full duration-300 ${shadowNavbar ? "shadow-active" : " "}`}>
+      <div className="max-w-6xl mx-auto p-4 sm:px-6 lg:px-8">
+        <div className="flex items-center justify-between h-10">
           {/* kiri */}
           <div className="flex">
-            <div className="flex items-center w-10 rounded-full">
+            <div className="flex items-center w-20 rounded-full">
               <Link to="/">
                 anggap saja logo
                 {/* <img src={ppSaya} alt="Avatar" className="rounded-full" /> */}
@@ -31,32 +48,32 @@ const Navbar = () => {
 
           {/* <div className="hidden md:block"> */}
           <div className="">
-            <div className="ml-5 flex items-baseline space-x-4">
+            <div className="ml-5 flex items-baseline">
               <Link
-                to={`/auth`}
-                className="text-2xl text-gray-800 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md font-medium"
+                to={`/ auth`}
+                className="text-lg text-gray-800 hover:bg-gray-700 hover:text-white px-4 py-2 rounded-md font-medium transition duration-200 ease-in-out mx-5"
               >
                 <div className="flex items-center">
                   {/* <BsPersonCircle className="mr-2" /> */}
-                  <span className="align-middle">Authentication</span>
+                  <span className="align-middle font-normal">Authentication</span>
                 </div>
               </Link>
               <Link
                 to={'/report'}
-                className="text-2xl text-gray-800 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md font-medium"
+                className="text-lg text-gray-800 hover:bg-gray-700 hover:text-white px-4 py-2 rounded-md font-medium transition duration-200 ease-in-out mx-5"
               >
                 <div className="flex items-center">
                   {/* <HiOutlineBeaker className="mr-2" /> */}
-                  <span className="align-middle">Report</span>
+                  <span className="align-middle  font-normal">Report</span>
                 </div>
               </Link>
               <Link
-                to={`/about`}
-                className="text-2xl text-gray-800 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md font-medium"
+                to={`/ about`}
+                className="text-lg text-gray-800 hover:bg-gray-700 hover:text-white px-4 py-2 rounded-md font-medium transition duration-200 ease-in-out mx-5"
               >
                 <div className="flex items-center">
                   {/* <BsPersonCircle className="mr-2" /> */}
-                  <span className="align-middle">About</span>
+                  <span className="align-middle  font-normal">About</span>
                 </div>
               </Link>
             </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,8 @@ import Layout from './Layout';
 const Home = () => {
   return (
     <Layout>
-      <div>ini buat home</div>
+      <div className='min-h-screen pt-16 flex justify-center items-center'>ini buat home</div>
+      <div className='min-h-screen pt-16 flex justify-center items-center'>ini buat test navbar</div>
     </Layout>
   );
 };
