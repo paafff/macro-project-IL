521965af0bb6d9256674dca9a56fef831f1b189f
paafff <danang050402@gmail.com>
2023-10-09 23:04:48 +0700

    Squashed commit of the following:
    commit 908c2ae074a61f74a2ce20028c85eeaebd5f7037
    Author: IlhamSoejudAlkahfiardy <ilhamyusuf386@gmail.com>
    Date:   Mon Oct 9 22:57:26 2023 +0700
    
        ngefix icon about, foto profil developer, state biodata, tanggal card pengaduan

--- src/pages/Pengaduan.jsx
+++ src/pages/Pengaduan.jsx
@@ -36,12 +36,205 @@ const Pengaduan = () => {
         <div className="min-h-fit 2xl:max-w-[60%] xl:max-w-[75%] xl:mx-auto xl:flex xl:flex-wrap grid grid-cols-1 md:grid-cols-2 gap-8 p-2 pt-20 mb-24">
           {/* Card data dummy */}
 
-          <div className="md:aspect-[12/16] p-5 h-auto drop-shadow-lg xl:my-6 lg:w-80 lg:mx-auto lg:my-auto md:my-auto bg-slate-50 rounded-lg shadow-sm flex flex-col ">
-            <div className="flex flex-col justify-between w-full items-center gap-4">
-              <div className="flex justify-between w-full">
-                <p className="text-sm w-1/2 font-medium">Laporan 1</p>
-                <div className="flex gap-2 items-center">
-                  <div className="h-4 w-4 rounded-full bg-[#F7C873]"></div>
+                <div className="min-h-fit 2xl:max-w-[60%] xl:max-w-[75%] xl:mx-auto xl:flex xl:flex-wrap grid grid-cols-1 md:grid-cols-2 gap-8 p-2 pt-20 mb-24">
+
+                    {/* Card data dummy */}
+
+                    <div
+                        className="md:aspect-[12/16] p-5 h-auto drop-shadow-lg xl:my-6 lg:w-80 lg:mx-auto lg:my-auto md:my-auto bg-slate-50 rounded-lg shadow-sm flex flex-col ">
+
+                        <div className="flex flex-col justify-between w-full items-center gap-4">
+                            <div className='flex justify-between w-full'>
+                                <p className='text-sm w-1/2 font-medium'>Laporan 1</p>
+                                <div className='flex gap-2 items-center'>
+
+                                    <div className='h-4 w-4 rounded-full bg-[#F7C873]'>
+
+                                    </div>
+
+                                    <p className='text-sm font-thin'>Pending</p>
+                                </div>
+                            </div>
+                            <div className='flex w-full flex-wrap'>
+                                <p className='mb-2 text-sm'>Pelapor</p>
+                                <div class="relative h-10 w-full min-w-[200px]">
+                                    <input
+                                        class="peer w-full rounded-[5px] border border-gray-300 px-3 py-2.5 text-sm font-normal text-blue-gray-300 outline outline-0 transition-all "
+                                        placeholder=" "
+                                    />
+                                </div>
+                            </div>
+                            <div className='flex w-full flex-wrap'>
+                                <p className='mb-2 text-sm'>Korban</p>
+                                <div class="relative h-10 w-full min-w-[200px]">
+                                    <input
+                                        class="peer w-full rounded-[5px] border border-gray-300 px-3 py-2.5 text-sm font-normal text-blue-gray-300 outline outline-0 transition-all "
+                                        placeholder=" "
+                                    />
+                                </div>
+                            </div>
+                            <div className='flex w-full flex-wrap'>
+                                <p className='mb-2 text-sm'>Kronologi</p>
+                                <div class="relative h-32 w-full min-w-[200px]">
+                                    <textarea
+                                        class="peer h-full min-h-[100px] w-full resize-none rounded-[5px] border border-blue-gray-200 px-3 py-2.5 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-pink-500 focus:outline-0 disabled:resize-none disabled:border-0 disabled:bg-blue-gray-50"
+                                        placeholder=" "
+                                    ></textarea>
+                                </div>
+                            </div>
+                            <p className='text-sm text-gray-400 text-end'>01 - 10 - 2023</p>
+                        </div>
+                    </div>
+
+                    <div
+                        className="md:aspect-[12/16] p-5 h-auto drop-shadow-lg xl:my-6 lg:w-80 lg:mx-auto lg:my-auto md:my-auto bg-slate-50 rounded-lg shadow-sm flex flex-col ">
+
+                        <div className="flex flex-col justify-between w-full items-center gap-4">
+                            <div className='flex justify-between w-full'>
+                                <p className='text-sm w-1/2 font-medium'>Laporan 2</p>
+                                <div className='flex gap-2 items-center'>
+
+                                    <div className='h-4 w-4 rounded-full bg-[#F7C873]'>
+
+                                    </div>
+
+                                    <p className='text-sm font-thin'>Reject</p>
+                                </div>
+                            </div>
+                            <div className='flex w-full flex-wrap'>
+                                <p className='mb-2 text-sm'>Pelapor</p>
+                                <div class="relative h-10 w-full min-w-[200px]">
+                                    <input
+                                        class="peer w-full rounded-[5px] border border-gray-300 px-3 py-2.5 text-sm font-normal text-blue-gray-300 outline outline-0 transition-all "
+                                        placeholder=" "
+                                    />
+                                </div>
+                            </div>
+                            <div className='flex w-full flex-wrap'>
+                                <p className='mb-2 text-sm'>Korban</p>
+                                <div class="relative h-10 w-full min-w-[200px]">
+                                    <input
+                                        class="peer w-full rounded-[5px] border border-gray-300 px-3 py-2.5 text-sm font-normal text-blue-gray-300 outline outline-0 transition-all "
+                                        placeholder=" "
+                                    />
+                                </div>
+                            </div>
+                            <div className='flex w-full flex-wrap'>
+                                <p className='mb-2 text-sm'>Kronologi</p>
+                                <div class="relative h-32 w-full min-w-[200px]">
+                                    <textarea
+                                        class="peer h-full min-h-[100px] w-full resize-none rounded-[5px] border border-blue-gray-200 px-3 py-2.5 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-pink-500 focus:outline-0 disabled:resize-none disabled:border-0 disabled:bg-blue-gray-50"
+                                        placeholder=" "
+                                    ></textarea>
+                                </div>
+                            </div>
+                            <p className='text-sm text-gray-400 text-end'>09 - 10 - 2023</p>
+                        </div>
+                    </div>
+
+                    <div
+                        className="md:aspect-[12/16] p-5 h-auto drop-shadow-lg xl:my-6 lg:w-80 lg:mx-auto lg:my-auto md:my-auto bg-slate-50 rounded-lg shadow-sm flex flex-col ">
+
+                        <div className="flex flex-col justify-between w-full items-center gap-4">
+                            <div className='flex justify-between w-full'>
+                                <p className='text-sm w-1/2 font-medium'>Laporan 3</p>
+                                <div className='flex gap-2 items-center'>
+
+                                    <div className='h-4 w-4 rounded-full bg-[#F7C873]'>
+
+                                    </div>
+
+                                    <p className='text-sm font-thin'>Success</p>
+                                </div>
+                            </div>
+                            <div className='flex w-full flex-wrap'>
+                                <p className='mb-2 text-sm'>Pelapor</p>
+                                <div class="relative h-10 w-full min-w-[200px]">
+                                    <input
+                                        class="peer w-full rounded-[5px] border border-gray-300 px-3 py-2.5 text-sm font-normal text-blue-gray-300 outline outline-0 transition-all "
+                                        placeholder=" "
+                                    />
+                                </div>
+                            </div>
+                            <div className='flex w-full flex-wrap'>
+                                <p className='mb-2 text-sm'>Korban</p>
+                                <div class="relative h-10 w-full min-w-[200px]">
+                                    <input
+                                        class="peer w-full rounded-[5px] border border-gray-300 px-3 py-2.5 text-sm font-normal text-blue-gray-300 outline outline-0 transition-all "
+                                        placeholder=" "
+                                    />
+                                </div>
+                            </div>
+                            <div className='flex w-full flex-wrap'>
+                                <p className='mb-2 text-sm'>Kronologi</p>
+                                <div class="relative h-32 w-full min-w-[200px]">
+                                    <textarea
+                                        class="peer h-full min-h-[100px] w-full resize-none rounded-[5px] border border-blue-gray-200 px-3 py-2.5 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-pink-500 focus:outline-0 disabled:resize-none disabled:border-0 disabled:bg-blue-gray-50"
+                                        placeholder=" "
+                                    ></textarea>
+                                </div>
+                            </div>
+                            <p className='text-sm text-gray-400 text-end'>23 - 10 - 2023</p>
+                        </div>
+                    </div>
+
+                    {/* Card data dummy */}
+
+                    {/* Card data real laporan */}
+
+                    {reports.map((report, index) => (
+
+                        <div
+                            className="md:aspect-[12/16] p-5 h-auto drop-shadow-lg xl:my-6 lg:w-80 lg:mx-auto lg:my-auto md:my-auto bg-slate-50 rounded-lg shadow-sm flex flex-col ">
+
+                            <Link to={`/report/info/${index}`}>
+                                <div className="flex flex-col justify-between w-full items-center gap-4">
+                                    <div className='flex justify-between w-full'>
+                                        <p className='text-sm w-1/2 font-medium'>{report.judulLaporan}</p>
+                                        <div className='flex gap-2 items-center'>
+
+                                            <div className='h-4 w-4 rounded-full bg-[#F7C873]'>
+
+                                            </div>
+
+                                            <p className='text-sm font-thin'>Pending</p>
+                                        </div>
+                                    </div>
+                                    <div className='flex w-full flex-wrap'>
+                                        <p className='mb-2 text-sm'>Pelapor</p>
+                                        <div class="relative h-10 w-full min-w-[200px]">
+                                            <input
+                                                class="peer w-full rounded-[5px] border border-gray-300 px-3 py-2.5 text-sm font-normal text-blue-gray-300 outline outline-0 transition-all "
+                                                placeholder=" "
+                                            />
+                                        </div>
+                                    </div>
+                                    <div className='flex w-full flex-wrap'>
+                                        <p className='mb-2 text-sm'>Korban</p>
+                                        <div class="relative h-10 w-full min-w-[200px]">
+                                            <input
+                                                class="peer w-full rounded-[5px] border border-gray-300 px-3 py-2.5 text-sm font-normal text-blue-gray-300 outline outline-0 transition-all "
+                                                placeholder=" "
+                                            />
+                                        </div>
+                                    </div>
+                                    <div className='flex w-full flex-wrap'>
+                                        <p className='mb-2 text-sm'>Kronologi</p>
+                                        <div class="relative h-32 w-full min-w-[200px]">
+                                            <textarea
+                                                class="peer h-full min-h-[100px] w-full resize-none rounded-[5px] border border-blue-gray-200 px-3 py-2.5 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-pink-500 focus:outline-0 disabled:resize-none disabled:border-0 disabled:bg-blue-gray-50"
+                                                placeholder=" "
+                                            ></textarea>
+                                        </div>
+                                    </div>
+                                </div>
+                            </Link>
+                        </div>
+
+
+                    ))}
+
+                    {/* Card data real laporan */}
 
                   <p className="text-sm font-thin">Pending</p>
                 </div>
