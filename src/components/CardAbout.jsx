99cd16da3105dcfc32b6b7772a1004309274e79b
IlhamSoejudAlkahfiardy <ilhamyusuf386@gmail.com>
2023-10-10 18:08:38 +0700

    update card developer about

--- src/components/CardAbout.jsx
+++ src/components/CardAbout.jsx
@@ -5,6 +5,7 @@ import ImageRafael from '../assets/image/profile_picture/Rafael_Kesma.png';
 import ImageIlham from '../assets/image/profile_picture/ilham(2).png';
 import ImageTania from '../assets/image/profile_picture/tania.jpg';
 import ImageGilang from '../assets/image/profile_picture/gilang.jpg';
+import ImageAdit from '../assets/image/profile_picture/adit.jpg';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
@@ -53,26 +54,25 @@ function CardAbout() {
               />
               <FontAwesomeIcon
                 onClick={() =>
-                  link('https://github.com/IlhamSoejudAlkahfiardy')
+                  link('https://github.com/rafrap03')
                 }
                 className="hover:scale-150 transition ease-in-out hover:cursor-pointer"
                 size="lg"
                 icon={faGithub}
               />
               <FontAwesomeIcon
-                onClick={() =>
-                  link('https://github.com/IlhamSoejudAlkahfiardy')
-                }
+
                 className="hover:scale-150 transition ease-in-out hover:cursor-pointer"
                 size="lg"
-                icon={faGithub}
+                style={{ color: '#2071fe' }}
+                icon={faFacebook}
               />
             </div>
           </div>
           <div className="md:aspect-[12/16] shadow-xl  xl:my-6 lg:w-80 lg:mx-auto lg:my-auto md:my-auto bg-slate-50 rounded-lg shadow-sm flex flex-col ">
             <div className="bg-slate-50 h-3/4 rounded-lg flex justify-center items-center">
               <img
-                className="w-24"
+                className="w-32"
                 src={ImageIlham}
                 alt="profile of ilham"
               />
@@ -106,9 +106,7 @@ function CardAbout() {
               />
 
               <FontAwesomeIcon
-                onClick={() =>
-                  link('https://www.linkedin.com/in/ilhamsoejudalkahfiardy/')
-                }
+
                 className="hover:scale-150 transition ease-in-out hover:cursor-pointer"
                 size="lg"
                 icon={faFacebook}
@@ -161,7 +159,7 @@ function CardAbout() {
           </div>
           <div className="md:aspect-[12/16] shadow-xl  xl:my-6 lg:w-80 lg:mx-auto lg:my-auto md:my-auto bg-slate-50 rounded-lg shadow-sm flex flex-col ">
             <div className="bg-slate-50 h-3/4 rounded-lg flex justify-center items-center">
-              <img className=" w-32" src={ImageProfile} alt="" />
+              <img className=" w-32" src={ImageAdit} alt="" />
             </div>
             <div className="text-center px-2">
               <p className="font-semibold text-base text-blue-950 tracking-wide">
@@ -175,7 +173,7 @@ function CardAbout() {
             </div>
             <div className="my-4 flex justify-center gap-4 pb-4">
               <FontAwesomeIcon
-                onClick={() => link('http://instagram.com/ilhamsoejud')}
+                onClick={() => link('https://instagram.com/adtyanur?igshid=OGQ5ZDc2ODk2ZA==')}
                 className="hover:scale-150 transition ease-in-out hover:cursor-pointer"
                 size="lg"
                 icon={faInstagram}
@@ -183,9 +181,7 @@ function CardAbout() {
               />
 
               <FontAwesomeIcon
-                onClick={() =>
-                  link('https://github.com/IlhamSoejudAlkahfiardy')
-                }
+                onClick={() => link('https://github.com/adtyanur')}
                 className="hover:scale-150 transition ease-in-out hover:cursor-pointer"
                 size="lg"
                 icon={faGithub}
@@ -193,7 +189,7 @@ function CardAbout() {
 
               <FontAwesomeIcon
                 onClick={() =>
-                  link('https://www.linkedin.com/in/ilhamsoejudalkahfiardy/')
+                  link('https://www.facebook.com/IAditTHEkInGEPfp?mibextid=ZbWKwL')
                 }
                 className="hover:scale-150 transition ease-in-out hover:cursor-pointer"
                 size="lg"
@@ -218,7 +214,7 @@ function CardAbout() {
             </div>
             <div className="my-4 flex justify-center gap-4 pb-4">
               <FontAwesomeIcon
-                onClick={() => link('http://instagram.com/ilhamsoejud')}
+                onClick={() => link('http://instagram.com/taniaftrr_')}
                 className="hover:scale-150 transition ease-in-out hover:cursor-pointer"
                 size="lg"
                 icon={faInstagram}
@@ -226,18 +222,14 @@ function CardAbout() {
               />
 
               <FontAwesomeIcon
-                onClick={() =>
-                  link('https://github.com/IlhamSoejudAlkahfiardy')
-                }
+
                 className="hover:scale-150 transition ease-in-out hover:cursor-pointer"
                 size="lg"
                 icon={faGithub}
               />
 
               <FontAwesomeIcon
-                onClick={() =>
-                  link('https://www.linkedin.com/in/ilhamsoejudalkahfiardy/')
-                }
+
                 className="hover:scale-150 transition ease-in-out hover:cursor-pointer"
                 size="lg"
                 icon={faFacebook}
@@ -261,7 +253,7 @@ function CardAbout() {
             </div>
             <div className="my-4 flex justify-center gap-4 pb-4">
               <FontAwesomeIcon
-                onClick={() => link('http://instagram.com/ilhamsoejud')}
+                onClick={() => link('https://www.instagram.com/alwafi1503/')}
                 className="hover:scale-150 transition ease-in-out hover:cursor-pointer"
                 size="lg"
                 icon={faInstagram}
@@ -270,7 +262,7 @@ function CardAbout() {
 
               <FontAwesomeIcon
                 onClick={() =>
-                  link('https://github.com/IlhamSoejudAlkahfiardy')
+                  link('https://github.com/alwafi1503')
                 }
                 className="hover:scale-150 transition ease-in-out hover:cursor-pointer"
                 size="lg"
@@ -278,9 +270,7 @@ function CardAbout() {
               />
 
               <FontAwesomeIcon
-                onClick={() =>
-                  link('https://www.linkedin.com/in/ilhamsoejudalkahfiardy/')
-                }
+                onClick={() => link('https://web.facebook.com/gilang.alwafi.9')}
                 className="hover:scale-150 transition ease-in-out hover:cursor-pointer"
                 size="lg"
                 icon={faFacebook}
